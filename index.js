d134014a5363f85f1934b81fe8e11319ea1b2bb3
Siddhesh Rao <119157669+siddhesh-rao@users.noreply.github.com>
2025-12-30 13:07:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+//document.querySelectorAll("button")[0].addEventListener("click", gotClicked);
+
+// function gotClicked(){
+//     alert("I got clicked!");
+// }
+
+
+//By using anonymous function-function without a name
+var drumCount= document.querySelectorAll(".drum").length;
+
+for(var i=0; i<drumCount; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        makeSounds(this.innerHTML);
+    });
+}
+
+
+document.addEventListener("keydown", function(event){
+    makeSounds(event.key);
+});
+
+
+function makeSounds(key){
+    switch (key) {
+    case "w":
+        var tom1= new Audio('./sounds/tom-1.mp3');
+        tom1.play();
+    break;
+    case "a":
+        var tom2= new Audio('./sounds/tom-2.mp3');
+        tom2.play();
+    break;
+    case "s":
+        var tom3= new Audio('./sounds/tom-3.mp3');
+        tom3.play();
+    break;
+    case "d":
+        var tom4= new Audio('./sounds/tom-4.mp3');
+        tom4.play();
+    break;
+    case "j":
+        var snare= new Audio('./sounds/snare.mp3');
+        snare.play();
+    break;
+    case "k":
+        var crash= new Audio('./sounds/kick-bass.mp3');
+        crash.play();
+    break;
+    case "l":
+        var kick= new Audio('./sounds/crash.mp3');
+        kick.play();
+    break;
+    default: console.log(this.innerHTML);
+    };
+}
+
+
+//to play sound in js.
+// var audio1 = new Audio('./sounds/tom-1.mp3');
+// audio1.play();
+
+
+//jo key press karenge uska color white hojayega (uper anonymous) function ke andar.
+//this.style.color="white";
+
+
+
+
